f0036c5dccf1ededcb1b7bf168729800ca04f7af
Alessio Brunello <alessio.brunello@h-farm.com>
2022-05-03 09:25:42 +0200

    added frontend auth

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+//import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
-function App() {
+
+function App({isPassedToWithAuthenticator, signOut, user}) {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h1>We now have Auth!</h1>
       </header>
+      <button onClick={signOut}>Sign out</button>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
